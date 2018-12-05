ef7bf8c0f80f7d52e0443cb413c81ea235f6cea2
gbellini90 <gbellini90@gmail.com>
2018-12-05 15:58:42 -0500

    work on search-incomplete

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -2,12 +2,26 @@ class Concert < ApplicationRecord
   has_many :tickets
   has_many :squads, through: :tickets
 
-  def self.search(artist)
-    if artist
-      where('artist LIKE ?', "%#{artist}%")
-      else
-        Concert.all
-      end
+  # def self.search(artist)
+  #   if artist
+  #     where('artist LIKE ?', "%#{artist}%")
+  #     else
+  #       Concert.all
+  #     end
+  #   end
+
+
+def self.search(artist)
+  if artist
+    found_artist = self.find_by(artist: artist)
+    if found_artist
+      self.where(concert_id: found_artist)
+    else
+      self.all
     end
+  else
+    self.all
+  end
+end
 
 end
