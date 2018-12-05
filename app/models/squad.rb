46c101cc4357ae408e1f677ffcf4b9c94afca4d4
jbmenashi <jbmenashi@gmail.com>
2018-12-05 10:04:40 -0500

    add controller and views for squad

--- app/models/squad.rb
+++ app/models/squad.rb
@@ -1,4 +1,7 @@
 class Squad < ApplicationRecord
   has_many :tickets
   has_many :squads, through: :tickets
+  has_many :photos
+  has_many :user_squads
+  has_many :users, through: :user_squads
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :tickets
+  resources :tickets, only: [:show]
   resources :concerts, only: [:index, :show]
-  resources :squads
+  resources :squads, only: [:show, :edit, :update]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
