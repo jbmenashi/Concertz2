972aa7f087ae84a0f1ea7be16a3213ff819be989
jbmenashi <jbmenashi@gmail.com>
2018-12-05 13:04:55 -0500

    add env

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Concert.destroy_all
+
+request = RestClient.get("https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=345&size=200&apikey=")
+response = JSON.parse(request)
+
+response["_embedded"]["events"].each do |event|
+  Concert.create(artist: event["name"], location: event["_embedded"]["venues"][0]["city"]["name"], venue: event["_embedded"]["venues"][0]["name"], genre: event["classifications"][0]["genre"]["name"])
+end
+
+
+
+
+byebug
+
+"hi"
+
+
+# ,
