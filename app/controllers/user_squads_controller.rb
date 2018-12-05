5e3872599496292c4a0d66c5ba93355e972a5691
jbmenashi <jbmenashi@gmail.com>
2018-12-05 17:23:30 -0500

    add more changes

--- app/controllers/user_squads_controller.rb
+++ app/controllers/user_squads_controller.rb
@@ -1,2 +1,17 @@
 class UserSquadsController < ApplicationController
+
+  def new
+    @user_squad = UserSquad.new
+  end
+
+  def create
+    @user_squad = UserSquad.new
+    if @user_squad.save
+      redirect_to squad_path(@squad)
+    else
+      redirect_to new_user_squad_path
+    end
+  end
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :tickets, only: [:show]
   resources :concerts, only: [:index, :show]
   resources :squads, only: [:show, :new, :create, :edit, :update]
+  resources :user_squads, only: [:new, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
