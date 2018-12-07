5a5127faaf930bedb3914dd84b37cdb59ad283c4
gbellini90 <gbellini90@gmail.com>
2018-12-07 13:49:41 -0500

    registeur

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_action :authorized, only: [:login, :authorize, :new, :create]
 
   def index
     @users = User.all
@@ -13,23 +14,19 @@ class UsersController < ApplicationController
 
   def new
     @user=User.new
+    render layout: 'new'
   end
 
   def login
     render layout: 'login'
   end
 
-  def authorize
-    @user = User.find_or_create_by(username: params[:username])
-
-    session[:user_id] = @user.id
-    redirect_to user_path(@user)
-  end
-
-
   def create
     @user = User.create(user_params)
+
     if @user.valid?
+      # add session
+      login_user(@user)
       redirect_to user_path(@user)
     else
       flash[:errors] = @user.errors.full_messages
@@ -37,6 +34,16 @@ class UsersController < ApplicationController
     end
   end
 
+  def authorize
+    @user = User.find_or_create_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash[:notice] = "incorrect dummy"
+      redirect_to login_path
+  end
+end
 
   def edit
     @user = session_user
@@ -55,18 +62,10 @@ class UsersController < ApplicationController
   end
 
 
-  def destroy
-    @user.destroy
-    redirect to users_path
-  end
-
-
-
-
 private
 
 def user_params
-  params.require(:user).permit(:username, :password, :name, :bio, :age, :image)
+  params.require(:user).permit(:username, :password, :name, :bio, :image, :age)
 end
 
 def find_user
